b55eb966bd8c32ccec70eaba941e2aae08b6390e
Suhita Jahan Easha <suhitajahaneasha@gmail.com>
2026-05-02 17:51:36 +0600

    Update NewCollections.jsx
    deploy

--- Frontend/src/Components/NewCollections/NewCollections.jsx
+++ Frontend/src/Components/NewCollections/NewCollections.jsx
@@ -7,7 +7,7 @@ const NewCollections = () => {
   const [new_collections,setNew_collections]=useState([]);
 
   useEffect(()=>{
-       fetch('http://localhost:4000/newcollections')
+       fetch('http://crimson-backend-anwo.onrender.com/newcollections')
        .then((response)=>response.json())
        .then((data)=>setNew_collections(data));
     
