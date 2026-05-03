bdcb6d888370e71d014b7bd0bb5377460dd20681
Suhita Jahan Easha <suhitajahaneasha@gmail.com>
2026-05-04 01:38:41 +0600

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,9 @@ const cors=require("cors");
 const { styleText } = require("util");
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:'https://crimson-frontend-56ky.onrender.com'
+}));
 
 //database connection 
 mongoose.connect("mongodb+srv://suhitaofficial:suhitaofficial20@cluster0.c9d7ekl.mongodb.net/ecommerce")
