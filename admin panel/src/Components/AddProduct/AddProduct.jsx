63198bf7fe4a58dcd81f0caf764b41a9fb28b6c2
Suhita Jahan Easha <suhitajahaneasha@gmail.com>
2026-05-03 16:33:24 +0600

    Update AddProduct.jsx

--- admin panel/src/Components/AddProduct/AddProduct.jsx
+++ admin panel/src/Components/AddProduct/AddProduct.jsx
@@ -25,7 +25,7 @@ const AddProduct = () => {
         let formData=new FormData();
         formData.append('product',image);
 
-        await fetch('http://crimson-backend-anwo.onrender.com/upload',{
+        await fetch('http://crimson-backend-anwo.onrender.com//upload',{
             method:'POST',
             headers:{
                 Accept:'application/json',
@@ -37,7 +37,7 @@ const AddProduct = () => {
         if (responseData.success){
             product.image=responseData.image_url;
             console.log(product);
-            await fetch('http://crimson-backend-anwo.onrender.com/addproduct',{
+            await fetch('http://crimson-backend-anwo.onrender.com//addproduct',{
                 method:'POST',
                 headers:{
                     Accept:'application/json',
