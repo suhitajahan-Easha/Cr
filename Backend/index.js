3e8b82e9ed74a0d57af1b88892a361be08020d00
Suhita Jahan Easha <suhitajahaneasha@gmail.com>
2026-05-03 18:28:32 +0600

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,4 @@
-const port= 4000;
+const port= process.env.PORT || 4000;
 const express = require("express");
 const app =express();
 const mongoose= require("mongoose");
@@ -36,7 +36,7 @@ app.use('/images',express.static('upload/images'))
 app.post("/upload",upload.single('product'),(req,res)=>{
     res.json({
         success:1,
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
+        image_url:`${port}/images/${req.file.filename}`
     })
 })
 
